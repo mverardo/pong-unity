ed1717f34202a7db637933167a7f1c240711d474
Maurício Verardo <mauricio.verardo@gmail.com>
2021-01-16 17:27:49 -0300

    Reset the ball on goal

---
+++ Assets/BallReset.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReset : MonoBehaviour
+{
+    public void Reset()
+    {
+        var rb = GetComponent<Rigidbody2D>();
+        rb.MovePosition(Vector2.zero);
+    }
+}
