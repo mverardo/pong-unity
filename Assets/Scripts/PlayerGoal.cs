0f5b5ecc84b622b781d51679321baa535d18bbe4
Maurício Verardo <mauricio.verardo@gmail.com>
2021-01-16 17:23:34 -0300

    Keeping the score

---
+++ Assets/Scripts/PlayerGoal.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class PlayerGoal : MonoBehaviour
+{
+    // public GoalEvent OnGoal;
+
+    public UnityEvent OnGoal;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        this.OnGoal.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour
+{
+    public int score = 0;
+    public void GoalScored()
+    {
+        score++;
+        this.GetComponent<TextMeshProUGUI>().text = score.ToString();
+    }
+}
