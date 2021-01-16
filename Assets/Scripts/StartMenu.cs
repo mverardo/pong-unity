6e12d6f0bb7b26b2b3f0686acbd3bc2d2bc6b178
Maurício Verardo <mauricio.verardo@gmail.com>
2021-01-16 16:31:59 -0300

    Add StartMenu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public KeyCode startKey = KeyCode.Return;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(startKey))
+        {
+            SceneManager.LoadScene("PongScene");
+        }
+    }
+}
