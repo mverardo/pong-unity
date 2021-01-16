ef385188a0d674895d6255d0a3a0c74b3ba32785
Maurício Verardo <mauricio.verardo@gmail.com>
2021-01-16 15:50:25 -0300

    Add colliders and RigidBodies

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(1.0f, 1.0f).normalized * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
