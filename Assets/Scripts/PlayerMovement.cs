55d3160c0180bd6c170ac70f2859b1b238ed5a7c
Maurício Verardo <mauricio.verardo@gmail.com>
2021-01-16 16:00:46 -0300

    It works!

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public KeyCode up;
+    public KeyCode down;
+
+    public float speed = 3;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var velocity = new Vector2(0f, 0f);
+        if (Input.GetKey(up))
+        {
+            velocity = new Vector2(0f, speed);
+        }
+
+        if (Input.GetKey(down))
+        {
+            velocity = new Vector2(0f, -speed);
+        }
+        rb.velocity = velocity;
+    }
+}
